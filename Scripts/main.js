7e0e6cd1ca38139b47c7a27d59c9991d34902b8b
bugged-codes <vipul_49@outlook.com>
2022-12-10 02:42:45 +0530

    Few UI tewaks, Completed Problem Solving

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,3 +2,7 @@ function myFunction() {
 	document.getElementById("content-div").classList.toggle("main-content");
 	document.getElementById("button").classList.toggle("hide-text");
 }
+
+// function myFunction() {
+//	document.getElementById("content-div-parent").style.maxHeight = "max-content";
+// }
